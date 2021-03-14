ebd3799e1d427430f566fea7341245a95a7a165a
dineshkumarchaursiya <80622174+dineshkumarchaursiya@users.noreply.github.com>
2021-03-14 15:27:07 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+print("hello world")
+print("what is your name?")                 # asking user name
+my_name = input()
+print("Its nice metting you " + my_name)
+print("The length of your name is : ")
+print(len(my_name))
+print("What is your age?")                  # asking user age
+age = input()
+print("Your age is : " , str(int(age)))
+
+
+      
